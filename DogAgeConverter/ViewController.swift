4c279e7fc25f284e365363a9bdf4691ccc1f1aa6
16saadc <16saadc@16saadc-MBP-2.local>
2015-03-03 13:08:45 -0500

    first commit

--- DogAgeConverter/ViewController.swift
+++ DogAgeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageInDogYearsLabel: UILabel!
+    
+    @IBOutlet weak var ageInHumanYearsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let ageFromTextField = ageInHumanYearsTextField.text.toInt()!
+        
+        let conversionConstant = 7
+        
+        ageInDogYearsLabel.text = "\(ageFromTextField * conversionConstant)" + " In Dog Years"
+        
+        ageInDogYearsLabel.hidden = false
+        
+        ageInHumanYearsTextField.resignFirstResponder()
+    }
+  
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        let stringFromTextField = ageInHumanYearsTextField.text
+        let optionalDoubleFromTextField = Double((stringFromTextField as NSString).doubleValue)
+        
+            var realDogYears:Double
+        
+        if optionalDoubleFromTextField > 2 {
+            realDogYears = (10.5 * 2) + (optionalDoubleFromTextField - 2) * 4
+        }
+        else {
+            realDogYears = optionalDoubleFromTextField * 10.5
+        }
+        
+        ageInDogYearsLabel.text = "\(realDogYears)" + " In Real Dog Years"
+        ageInDogYearsLabel.hidden = false
+        
+        ageInHumanYearsTextField.resignFirstResponder()
+    }
+    
+    
 }
 
